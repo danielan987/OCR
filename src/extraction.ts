4268b7b94962ae84d762e60b70f18c91576e121f
Daniel An <118602647+danielan987@users.noreply.github.com>
2025-08-13 10:05:39 -0400

    Create extraction.ts

---
+++ src/extraction.ts
@@ -0,0 +1,52 @@
+import OpenAI from 'openai';
+import { EXTRACTION_MESSAGE } from './messages.js';
+import { extractionSchema } from './schema.js';
+import * as z from 'zod';
+
+const client = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+export async function extract(pdfBuffer: Buffer) {
+  const response = await client.responses.parse({
+    model: 'gpt-5-nano',
+    max_output_tokens: 5000,
+    instructions: 'You are a fast, cost-aware PDF extraction assistant.',
+    temperature: 0,
+    top_p: 0.1,
+    input: [
+      {
+        type: 'message',
+        role: 'user',
+        content: [
+          {
+            type: 'input_file',
+            filename: 'financial_statement.pdf',
+            file_data: `data:application/pdf;base64,${pdfBuffer.toString('base64')}`,
+          },
+          {
+            type: 'input_text',
+            text: EXTRACTION_MESSAGE(JSON.stringify(extractionSchema)),
+          },
+        ],
+      },
+    ],
+    text: {
+      format: {
+        type: 'json_schema',
+        name: 'extracted_data',
+        strict: true,
+        schema: z.toJSONSchema(
+          z.object({
+            results: extractionSchema,
+          }),
+        ),
+      },
+    },
+  });
+
+  return {
+    data: response.output_parsed,
+    usage: response.usage,
+  };
+}
